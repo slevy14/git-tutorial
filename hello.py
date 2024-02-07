a755cddd1d51d4345075c0566a5a9d4a31fef39b
slevy14 <sammyglevy@gmail.com>
2024-02-06 21:52:03 -0800

    add stashed changes

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ print("hello!")
 print("io triumphe!")
 print(9 + 10)
 testing.secondfile()
+
+a = 6
+b = 15
+print("the sum of", a, "and", b, "is", testing.add_two_nums(a,b))
--- testing.py
+++ testing.py
@@ -1,3 +1,7 @@
 def secondfile():
 	print("this is the main branch!")
 	print("now we're on a new branch!")
+	print("this function is from testing.py!")
+
+def add_two_nums(a, b):
+	return(a + b)
